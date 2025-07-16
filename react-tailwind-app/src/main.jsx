d028ead203c2d3134c58ff3fb1dc5eaa9903d72c
Nattapon Klakhaeng <nattapon.kl@kbtg.tech>
2025-07-16 13:47:19 +0700

    add login page

--- react-tailwind-app/src/main.jsx
+++ react-tailwind-app/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router';
+import App from './App.jsx';
+import LoginPage from './LoginPage.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/login', element: <LoginPage /> },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
